3fbc6ce9f2cfa0fa41e7a3c826b68312dbd5fb04
Marek <fx@marksoft.com.pl>
2021-01-30 01:00:37 +0100

    praca domowa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddingComponent } from './component/adding/adding.component';
+import { MultiplicationComponent } from './component/multiplication/multiplication.component';
+import { DivisionComponent } from './component/division/division.component';
+import { SubtractionComponent } from './component/subtraction/subtraction.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+    declarations: [
+        AppComponent,
+        AddingComponent,
+        MultiplicationComponent,
+        DivisionComponent,
+        SubtractionComponent,
+        SubtractionComponent
+    ],
   imports: [
     BrowserModule
   ],
